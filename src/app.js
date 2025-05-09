afe562a9d37005b3b3f0cb32522b42361d808377
Ganesh <machavarapuganesh2004@gmail.com>
2025-05-09 06:06:37 +0530

    http metods are applied

--- src/app.js
+++ src/app.js
@@ -3,17 +3,28 @@ const express=require("express");
 const app=express();
 
 
+
+app.get("/user",(req,res)=>{
+    res.send({"firstName":"ganesh","second name":"machavarapu"});
+})
+
+app.post("/user",(req,res)=>{
+    // console.log("deleting the user");
+    res.send("user is posted successsfully");
+    
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("user is deleted successfully");
+})
+
+
 app.use("/test",(req,res)=>{
     res.send("this is test page u know");
 });
 
 
-app.use("/",(req,res)=>{
-    res.send("this is home or dashboard");
-});
-// app.use((req,res)=>{
-//     res.send("this is first project"); //this is request hander
-// })
+
 app.listen(7777,()=>{
     console.log("server is successsfully running on the 7777"); 
 })
