9c2d16e387ee1fd888984d46e355bc04a4f86866
Ganesh <machavarapuganesh2004@gmail.com>
2025-05-14 04:11:38 +0530

    delete and patch API

--- src/app.js
+++ src/app.js
@@ -1,40 +1,73 @@
 const express=require("express");
 const {dbconncetion} = require("./config/databases");
 const {User} = require("./models/userschema");
+const { ReturnDocument } = require("mongodb");
 const app=express();
 
 app.use(express.json());
 
-app.get("/user",async (req,res)=>{
- try{
-     const usermail = req.body.email;
-     console.log(req.body);
-     
-    const finduser = await User.findOne({Email:usermail});
-    if(!finduser)
-    {
-     return res.send("no user found,plzmregister");
-    }
-     res.send(finduser);
- }catch{
-     res.status(404).send("error occured,plz try again later");
- }
-})
+app.patch("/user",async (req,res)=>{
+   try{
+          const userdata=req.body;
+          const userupdated = await User.findOneAndUpdate({Email:req.body.email},userdata);
+          console.log(userupdated);
+          if(!userupdated)
+          {
+              res.status(404).send("user not found ");
+          }
 
-app.get("/feed", async (req, res) => {
-  try {  
-    const finduser = await User.findById({_id:"682282168051c6b395dc13d6"}); // Ensure 'Email' matches DB field
-    
-    if(finduser.length==0)
-    {
-      res.send("no user found,plz register");
-    }
-    res.send(finduser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error occurred");
-  }
+          res.send("succesfully updated");
+   }catch(err)
+   {
+      res.status(505).send("something went wrong ! plz try again");
+   }
+
 });
+
+app.delete("/user",async (req,res)=>{
+   try{
+      const userid=req.body.id;
+      const deleteduser= await User.findByIdAndDelete({_id:userid});
+      if(!deleteduser)
+      {
+          return res.send("user not found");
+      }
+      res.status(404).send("succssfully deleted the user");
+   }catch(err){
+    res.status(505).send("something went wrong");
+   }
+});
+
+// app.get("/user",async (req,res)=>{
+//  try{
+//      const usermail = req.body.email;
+//      console.log(req.body);
+     
+//     const finduser = await User.findOne({Email:usermail});
+//     if(!finduser)
+//     {
+//      return res.send("no user found,plzmregister");
+//     }
+//      res.send(finduser);
+//  }catch{
+//      res.status(404).send("error occured,plz try again later");
+//  }
+// })
+
+// app.get("/feed", async (req, res) => {
+//   try {  
+//     const finduser = await User.findById({_id:"682282168051c6b395dc13d6"}); // Ensure 'Email' matches DB field
+    
+//     if(finduser.length==0)
+//     {
+//       res.send("no user found,plz register");
+//     }
+//     res.send(finduser);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Error occurred");
+//   }
+// });
 
 // app.post("/user",async (req,res)=>{
 //     // userobj={
