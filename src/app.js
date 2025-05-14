1a6450ef6ebd9514a2b0e5042b527f21dac5a21c
Ganesh <machavarapuganesh2004@gmail.com>
2025-05-14 05:37:44 +0530

    validations-on-db-schema

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.patch("/user",async (req,res)=>{
    try{
           const userdata=req.body;
-          const userupdated = await User.findOneAndUpdate({Email:req.body.email},userdata);
+          const userupdated = await User.findOneAndUpdate({Email:req.body.Email},userdata,{runValidators:true});
           console.log(userupdated);
           if(!userupdated)
           {
@@ -38,58 +38,58 @@ app.delete("/user",async (req,res)=>{
    }
 });
 
-// app.get("/user",async (req,res)=>{
-//  try{
-//      const usermail = req.body.email;
-//      console.log(req.body);
+app.get("/user",async (req,res)=>{
+ try{
+     const usermail = req.body.email;
+     console.log(req.body);
      
-//     const finduser = await User.findOne({Email:usermail});
-//     if(!finduser)
-//     {
-//      return res.send("no user found,plzmregister");
-//     }
-//      res.send(finduser);
-//  }catch{
-//      res.status(404).send("error occured,plz try again later");
-//  }
-// })
+    const finduser = await User.findOne({Email:usermail});
+    if(!finduser)
+    {
+     return res.send("no user found,plzmregister");
+    }
+     res.send(finduser);
+ }catch{
+     res.status(404).send("error occured,plz try again later");
+ }
+})
 
-// app.get("/feed", async (req, res) => {
-//   try {  
-//     const finduser = await User.findById({_id:"682282168051c6b395dc13d6"}); // Ensure 'Email' matches DB field
+app.get("/feed", async (req, res) => {
+  try {  
+    const finduser = await User.findById({_id:"682282168051c6b395dc13d6"}); // Ensure 'Email' matches DB field
     
-//     if(finduser.length==0)
-//     {
-//       res.send("no user found,plz register");
-//     }
-//     res.send(finduser);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error occurred");
-//   }
-// });
+    if(finduser.length==0)
+    {
+      res.send("no user found,plz register");
+    }
+    res.send(finduser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred");
+  }
+});
 
-// app.post("/user",async (req,res)=>{
-//     // userobj={
-//     //     firstName:"Surya",
-//     //     secondName:"Machavarapu",   //this is the instance of the user
-//     //     email:"machavarapusurya2004",
-//     //     password:"23456",
-//     //     age:18
-//     // }
+app.post("/user",async (req,res)=>{
+    // userobj={
+    //     firstName:"Surya",
+    //     secondName:"Machavarapu",   //this is the instance of the user
+    //     email:"machavarapusurya2004",
+    //     password:"23456",
+    //     age:18
+    // }
     
     
-//    const user =new User(req.body);
-//    try{
-//       await user.save();
-//      res.send("succssfully added the user details");
-//    }catch(err)
-//    {
-//      res.status(404).send("user data not updated");
+   const user =new User(req.body);
+   try{
+      await user.save();
+     res.send("succssfully added the user details");
+   }catch(err)
+   {
+     res.status(404).send("user data not posted"+err.message);
      
-//    }
+   }
 
-// });
+});
 dbconncetion().then(()=>{
     console.log("database connection is estalbished");
     app.listen(7777,()=>{
--- src/models/userschema.js
+++ src/models/userschema.js
@@ -2,19 +2,55 @@ const mongoose = require("mongoose");
 
 const userSchema  = mongoose.Schema({
     firstName :{
-        type:String
+        type:String,
+        required:true
     },secondName:{
         type:String
     },Email:{
-        type:String
+        type:String,
+        unique:true,
+        required:true
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:80
+
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
+    },
+    photoUrl:{
+        type:String,
+        default:"https://icon-library.com/images/user-png-icon/user-png-icon-16.jpg",
+    },
+    about:{
+        type:String,
+        defalut:"this is for biosection,required to update plz"
+        
+    },
+    gender:{
+
+        type:String,
+        validate(value)
+        {
+            if(!["male","female","others"].includes(value))
+            {
+                throw new error;
+            }
+        }
+
+    },
+    skills:{
+        type:[String]
     }
-});
+
+
+
+},
+ {timestamps: true});
 
 const User=mongoose.model("User",userSchema);
 module.exports={User};
