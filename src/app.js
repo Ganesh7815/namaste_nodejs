c5a6c40dd741f188bfa05c41205d536f72617f96
Ganesh <154363116+Ganesh7815@users.noreply.github.com>
2025-06-05 13:53:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 7777 ;
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",credentials:true},
+app.use(cors({origin:"https://namastte-web.vercel.app/",credentials:true},
       
 ));
 
